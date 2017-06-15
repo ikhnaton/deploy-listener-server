55b6cae52d51292ca1f2781cd8fd6c03a211c354
Niall Cargill <ikhnaton@us.ibm.com>
2017-06-15 14:10:15 -0700

    wrap it all in a promise

--- index.js
+++ index.js
@@ -67,121 +67,132 @@ _.forEach(config.handlers, (handler) =>
 {
 	github.on(`push:${handler.repo}:refs/heads/${handler.branch}`, function (data)
 	{
-		log.debug(`Push to the ${handler.branch} branch: `, (new Date()).toDateString());
-
-		log.debug(`by ${(data.head_commit.committer.name != "GitHub Enterprise")?data.head_commit.committer.name:data.head_commit.author.name} (${(data.head_commit.committer.email = "noreply@github.ibm.com")?data.head_commit.committer.email:data.head_commit.author.email})`);
-		log.debug("message:", data.head_commit.message);
-		log.debug("items:");
-		_.forEach(data.head_commit.modified, (file) =>
+		let p = new Promise((resolve, reject) =>
 		{
-			log.debug("   ", file);
+			log.debug(`Push to the ${handler.branch} branch: `, (new Date()).toDateString());
+
+			log.debug(`by ${(data.head_commit.committer.name != "GitHub Enterprise")?data.head_commit.committer.name:data.head_commit.author.name} (${(data.head_commit.committer.email == "noreply@github.ibm.com")?data.head_commit.committer.email:data.head_commit.author.email})`);
+			log.debug("message:", data.head_commit.message);
+			log.debug("items:");
+			_.forEach(data.head_commit.modified, (file) =>
+			{
+				log.debug("   ", file);
+			});
+
+			var execFile = require('child_process').execFile;
+			// params: file, args, options
+			execFile(handler.executable, null, null, function (error, stdout, stderr)
+			{
+				let slacker = ((handler.slackUrl != null) || (config.slackUrl != null)) ? true : false;
+
+				log.info("slacker: ", slacker);
+				// command output is in stdout
+				if (error)
+				{
+					log.info("error: ", error);
+					if (slacker == true)
+					{
+						slack.send("Deploy failed! " + error)
+							.then((res) =>
+							{
+								log.debug(
+								{
+									msg:
+									{
+										txt: "Slack message sent successfully",
+										result: res
+									}
+								});
+								resolve();
+							})
+							.catch((err) =>
+							{
+								log.error(
+								{
+									msg:
+									{
+										txt: "Slack message failed to send",
+										result: err
+									}
+								});
+								reject();
+							});
+					}
+					else { resolve(); }
+				}
+
+				if (stdout)
+				{
+					log.info("stdout: ", stdout);
+					if (slacker == true)
+					{
+						slack.send(`Deploy to branch, ${handler.branch}, successful!`)
+							.then((res) =>
+							{
+								log.debug(
+								{
+									msg:
+									{
+										txt: "Slack message sent successfully",
+										result: res
+									}
+								});
+								resolve();
+							})
+							.catch((err) =>
+							{
+								log.error(
+								{
+									msg:
+									{
+										txt: "Slack message failed to send",
+										result: err
+									}
+								});
+								reject();
+							});
+					}
+					else { resolve(); }
+				}
+
+				if (stderr)
+				{
+					log.info("stderr: ", stderr);
+					if (slacker == true)
+					{
+						slack.send("Deploy failed! " + stderr)
+							.then((res) =>
+							{
+								log.debug(
+								{
+									msg:
+									{
+										txt: "Slack message sent successfully",
+										result: res
+									}
+								});
+								resolve();
+							})
+							.catch((err) =>
+							{
+								log.error(
+								{
+									msg:
+									{
+										txt: "Slack message failed to send",
+										result: err
+									}
+								});
+								reject();
+							});
+					}
+					else { resolve(); }
+				}
+
+			});
 		});
 
-		var execFile = require('child_process').execFile;
-		// params: file, args, options
-		execFile(handler.executable, null, null, function (error, stdout, stderr)
-		{
-			let slacker = ((handler.slackUrl != null) || (config.slackUrl != null))?true:false;
-
-			log.info("slacker: ", slacker);
-			// command output is in stdout
-			if (error)
-			{
-				if (slacker == true)
-				{
-					log.info("Slack invoked");
-					slack.send("Deploy failed! " + error)
-						.then((res) =>
-						{
-							log.debug(
-							{
-								msg:
-								{
-									txt: "Slack message sent successfully",
-									result: res
-								}
-							});
-						})
-						.catch((err) =>
-						{
-							log.error(
-							{
-								msg:
-								{
-									txt: "Slack message failed to send",
-									result: err
-								}
-							});
-						});
-				}
-				log.info("error: ", error);
-			}
-
-			if (stdout)
-			{
-				if (slacker == true)
-				{
-					log.info("Slack invoked");
-					slack.send(`Deploy to branch, ${handler.branch}, successful!`)
-						.then((res) =>
-						{
-							log.debug(
-							{
-								msg:
-								{
-									txt: "Slack message sent successfully",
-									result: res
-								}
-							});
-						})
-						.catch((err) =>
-						{
-							log.error(
-							{
-								msg:
-								{
-									txt: "Slack message failed to send",
-									result: err
-								}
-							});
-						});
-				}
-				log.info("stdout: ", stdout);
-			}
-
-			if (stderr)
-			{
-				log.info("Slack invoked");
-				if (slacker == true)
-				{
-					slack.send("Deploy failed! " + stderr)
-						.then((res) =>
-						{
-							log.debug(
-							{
-								msg:
-								{
-									txt: "Slack message sent successfully",
-									result: res
-								}
-							});
-						})
-						.catch((err) =>
-						{
-							log.error(
-							{
-								msg:
-								{
-									txt: "Slack message failed to send",
-									result: err
-								}
-							});
-						});
-				}
-				log.info("error: ", error);
-			}
-
-		});
+		return p;
 	});
 });
 
